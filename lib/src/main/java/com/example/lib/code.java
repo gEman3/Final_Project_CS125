2ebaebe31ea05dd4cd1c0496f0f33c4e356c1ab6
Kevin Oh <sekeuno2@illinois.edu>
2019-04-30 02:22:49 -0500

    ey o

--- lib/src/main/java/com/example/lib/code.java
+++ lib/src/main/java/com/example/lib/code.java
@@ -2,30 +2,94 @@ package com.example.lib;
 import com.google.gson.*;
 
 public class code {
-    public static int getIDUsingName(java.lang.String json, String userInputName) {
+
+    public String info;
+
+
+    // Must use "search/flavor" API
+    public static String checkName(java.lang.String json, String userInputName) {
         if (userInputName == null) {
-            return 0;
+            return null;
         }
         JsonParser parser = new JsonParser();
         JsonArray result = parser.parse(json).getAsJsonArray();
         for (JsonElement strain : result) {
             String name = strain.getAsJsonObject().get("name").getAsString();
             if (name.equals(userInputName)) {
-                return strain.getAsJsonObject().get("id").getAsInt();
+                return strain.getAsJsonObject().get("name").getAsString();
             }
         }
-        return 0;
-    }
-    public static java.lang.String getRace(java.lang.String json, String race) {
         return null;
     }
-    public static java.lang.String getEffect(java.lang.String json, String effect) {
-        return null;
+
+
+    // Must use "search/all" API
+    public static String getName(java.lang.String json, String userInputName) {
+        String name = userInputName;
+        return name;
     }
-    public static java.lang.String getFlavor(java.lang.String json, String flavor) {
-        return null;
+
+    public static String getRace(java.lang.String json, String userInputName) {
+        JsonParser parser = new JsonParser();
+        JsonObject result = parser.parse(json).getAsJsonObject();
+        JsonObject metadata = result.getAsJsonObject(userInputName);
+        String race = metadata.get("race").getAsString();
+        return race;
     }
-    public static java.lang.String getAll(java.lang.String json) {
-        return null;
+
+    public static String getFlavor(java.lang.String json, String userInputName) {
+        JsonParser parser = new JsonParser();
+        JsonObject result = parser.parse(json).getAsJsonObject();
+        JsonObject metadata = result.getAsJsonObject(userInputName);
+        JsonArray flavors = metadata.get("flavors").getAsJsonArray();
+        String flavor = "";
+
+        for (JsonElement flavorIndividual : flavors) {
+            flavor = flavorIndividual.getAsString() + ", " + flavor;
+        }
+
+        return flavor;
+    }
+
+    public static String getPositiveEffect(java.lang.String json, String userInputName) {
+        JsonParser parser = new JsonParser();
+        JsonObject result = parser.parse(json).getAsJsonObject();
+        JsonObject metadata = result.getAsJsonObject(userInputName);
+        String positiveEffect = "";
+        JsonArray positiveEffects = metadata.getAsJsonObject("effects").getAsJsonArray("positive");
+
+        for (JsonElement effectIndividual : positiveEffects) {
+            positiveEffect = effectIndividual.getAsString() + ", " + positiveEffect;
+        }
+
+        return positiveEffect;
+    }
+
+    public static String getNegativeEffect(java.lang.String json, String userInputName) {
+        JsonParser parser = new JsonParser();
+        JsonObject result = parser.parse(json).getAsJsonObject();
+        JsonObject metadata = result.getAsJsonObject(userInputName);
+        String negativeEffect = "";
+        JsonArray negativeEffects = metadata.getAsJsonObject("effects").getAsJsonArray("negative");
+
+        for (JsonElement effectIndividual : negativeEffects) {
+            negativeEffect = effectIndividual.getAsString() + ", " + negativeEffect;
+        }
+
+        return negativeEffect;
+    }
+
+    public static String getMedicalEffect(java.lang.String json, String userInputName) {
+        JsonParser parser = new JsonParser();
+        JsonObject result = parser.parse(json).getAsJsonObject();
+        JsonObject metadata = result.getAsJsonObject(userInputName);
+        String medicalEffect = "";
+        JsonArray medicalEffects = metadata.getAsJsonObject("effects").getAsJsonArray("medical");
+
+        for (JsonElement effectIndividual : medicalEffects) {
+            medicalEffect = effectIndividual.getAsString() + ", " + medicalEffect;
+        }
+
+        return medicalEffect;
     }
 }
