ca53e7f2d44d6389b5345ca0242d684748cafdab
Kevin Oh <sekeuno2@illinois.edu>
2019-04-29 23:33:25 -0500

    ey o

--- app/src/main/java/com/example/final_project_cs125/MainActivity.java
+++ app/src/main/java/com/example/final_project_cs125/MainActivity.java
@@ -1,13 +1,51 @@
 package com.example.final_project_cs125;
 
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
+
+
+
+    private static final String TAG = "MP3:Main";
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        final Button searchButton = findViewById(R.id.button);
+        searchButton.setOnClickListener(v -> {
+            Log.d(TAG, "Button is pressed");
+            saveUserInput();
+        });
+
+        final TextInputLayout nameInput;
+        nameInput = (TextInputLayout) findViewById(R.id.NAME);
+        String name = nameInput.getEditText().getText().toString();
+        nameInput.setErrorEnabled(false);
+        nameInput.setError("Cant be blank");
+
+
+
+    }
+
+
+
+
+    public void saveUserInput() {
+        return;
+    }
+
+    public boolean isEmpty() {
+        //are 3 boxes empty and 1 box full;
+        return true;
     }
 }
--- lib/src/main/java/com/example/lib/code.java
+++ lib/src/main/java/com/example/lib/code.java
@@ -2,26 +2,26 @@ package com.example.lib;
 import com.google.gson.*;
 
 public class code {
-    public static java.lang.String getName(java.lang.String json) {
+    public static java.lang.String getName(java.lang.String json, String name) {
         if (json == null) {
             return null;
         }
         JsonParser parser = new JsonParser();
         JsonObject result = parser.parse(json).getAsJsonObject();
-        JsonObject metaDataObject = result.get("metadata").getAsJsonObject();
+        JsonObject metaDataObject = result.get(name).getAsJsonObject();
         if (metaDataObject.get("format") == null) {
             return null;
         }
         String format = metaDataObject.get("format").getAsString();
         return format;
     }
-    public static java.lang.String getRace(java.lang.String json) {
+    public static java.lang.String getRace(java.lang.String json, String race) {
         return null;
     }
-    public static java.lang.String getEffect(java.lang.String json) {
+    public static java.lang.String getEffect(java.lang.String json, String effect) {
         return null;
     }
-    public static java.lang.String getFlavor(java.lang.String json) {
+    public static java.lang.String getFlavor(java.lang.String json, String flavor) {
         return null;
     }
     public static java.lang.String getAll(java.lang.String json) {
